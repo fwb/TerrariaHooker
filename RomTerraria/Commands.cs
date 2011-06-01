43e7da8f0dc9c43390cb4847af7583c40844eb71
Adam McKay <fortehwin@gmail.com>
2011-06-01 15:59:02 +1000

    Moved most non-hook logic into Commands.cs, as any exception caused in usercode is masked by some crapass inherited crap that destroys the hook to protect the the memory.
    Moved some logic from HandleChatMsg into it's own specialised method.

---
+++ RomTerraria/Commands.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace RomTerraria
+{
+    class Commands
+    {
+        /// <summary>
+        /// Process a data packet, determining data type and where defined, 
+        /// calling additional handlers to break apart data into a more workable
+        /// format.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public static Packet ProcessData(byte[] data)
+        {
+            byte type = data[4];
+
+            string prefix;
+            string details = null;
+            var packet = new Packet(data, data.Length);
+
+            switch (type)
+            {
+                case 0x01:
+                    prefix = "USER LOGIN P1";
+                    break;
+                case 0x02:
+                    prefix = "USER LOGIN RELATED";
+                    break;
+                case 0x04:
+                    prefix = "USER LOGIN RELATED [CHARACTER DETAILS]";
+                    break;
+                case 0x05:
+                    prefix = "PLAYER UPDATE INVENTORY/EQUIP";
+                    break;
+                case 0x06:
+                    prefix = "PLAYER SEND COMPLETE [LOGIN]";
+                    break;
+                case 0x08:
+                    prefix = "PLAYER REQUEST TILE DATA";
+                    break;
+                case 0x0C:
+                    prefix = "PLAYER SYNC/GREET REQUEST";
+                    break;
+                case 0x0D:
+                    prefix = "PLAYER STATE CHANGE";
+                    //packet = new packet_PlayerState(data);
+                    break;
+                case 0x10:
+                    prefix = "PLAYER CURRENT/MAX HEALTH UPDATE";
+                    break;
+                case 0x13:
+                    prefix = "PLAYER USE DOOR";
+                    break;
+                case 0x16:
+                    prefix = "DETERMINE ITEM OWNER";    //UPDATE: apparently item[x].active defines whether the item is
+                    //active within the game world (outside of inventories). I assume
+                    //the shit in FindOwner is to determine who the new owner of a newly
+                    //dropped item is (owner is the current dropper, so they don't have
+                    //have preference on the item by being closest). Also seems to be called
+                    //when you try to pick up an item you don't have room for, which explains
+                    //the erroneus captures I was recieving.
+
+                    break;
+                case 0x18:
+                    prefix = "PLAYER HURT NPC";
+                    break;
+                case 0x19:
+                    prefix = "CHAT MESSAGE";
+                    packet = HandleChatMsg(data);   //pass data to handler. 
+                    //ALL handlers assume a Packet struct as input.
+                    //and ALL handlers return a Packet struct.
+
+                    break;
+                case 0x1A:
+                    prefix = "PLAYER HURT PLAYER";
+                    break;
+                case 0x1B:
+                    prefix = "PLAYER FIRED PROJECTILE";
+                    break;
+                case 0x1E:
+                    prefix = "PLAYER TOGGLED PVP";
+                    break;
+                case 0x1F:
+                    prefix = "PLAYER USE CHEST";
+                    break;
+                case 0x20:
+                    prefix = "?PLAYER PLACED CHEST?";
+                    break;
+                case 0x23:
+                    prefix = "PLAYER HEAL EFFECT";
+                    break;
+                case 0x24:
+                    prefix = "PLAYER ZONE UPDATE";
+                    break;
+                case 0x26:
+                    prefix = "SEND PASSWORD";
+                    break;
+                case 0x28:
+                    prefix = "PLAYER INTERACT NPC";
+                    break;
+                case 0x2A:
+                    prefix = "PLAYER CURRENT/MAX MANA UPDATE";
+                    break;
+                case 0x2c:
+                    prefix = "PLAYER DIED";
+                    break;
+                case 0x2d:
+                    prefix = "PLAYER PARTY UPDATE";
+                    break;
+                case 0x2F:
+                    prefix = "PLAYER USE/PLACE SIGN";
+                    break;
+                case 0x31:
+                    prefix = "PLAYER SPAWN";
+                    break;
+
+                default:
+                    prefix = "UNDEFINED";
+                    break;
+            }
+
+            var sBuffer = new StringBuilder();
+            foreach (byte t in data)
+            {
+                sBuffer.Append(Convert.ToInt32(t).ToString("x").PadLeft(2, '0') + " ");
+            }
+            //string s = System.Text.Encoding.ASCII.GetString(newBuffer);
+            //MakeItHarder.serverConsole.AddChatLine(sBuffer.ToString().ToUpper());
+
+            MakeItHarder.serverConsole.AddChatLine(prefix + " : :" + sBuffer.ToString().ToUpper());
+
+            return packet;
+
+        }
+
+        private static Packet HandleChatMsg(byte[] data)
+        {
+            var p = new packet_ChatMsg(data); //initialize packet class, populate fields from data
+            //...
+            //act on packet fields, generate new data etc.
+            var match = false;
+            if (p.Text[0] == 0x2F) // match backslash
+            {
+
+                var commands = p.Text.Split(' ');
+                switch (commands[0])
+                {
+                    case ("/meteor"):
+                        WorldEvents.SpawnMeteorCB();
+                        match = true;
+                        break;
+                    case ("/broadcast"):
+                        cmdBroadcast(commands, p);
+                        match = true;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            //create new Packet(data, length). use a length of -1 to indicate the packet is to be dropped.
+            //this will directly be used by WSASend so don't fuck it up!
+            //p.packet[10] = 0x7E;
+            if (match)
+                return new Packet(new byte[] { }, 0);
+
+            return new Packet(p.Packet, p.Packet.Length);
+        }
+
+        /// <summary>
+        /// Broadcast Command Handler
+        /// </summary>
+        /// <param name="commands">The array of space-delimited words</param>
+        /// <param name="p">Data class for ChatMsg</param>
+        private static void cmdBroadcast(string[] commands, packet_ChatMsg p)
+        {
+            if (commands.Length > 1)
+            {
+                string msg = null;
+                int i = 1;
+                while (i < commands.Length)
+                {
+                    msg = msg + " " + commands[i];
+                    i++;
+                }
+                NetMessage.SendData(25, -1, -1, msg, 8, 0xff, 0xcc, 0x66);
+            }
+            else
+            {
+                
+                NetMessage.SendData(25, p.PlayerId, -1, "USAGE: /echo <word>", 8, 0x99, 0xff, 0x99);
+            }
+        }
+    }
+
+    #region packet definitions
+    /// <summary>
+    /// classes for packet structures.
+    /// the packet parsing routines will create instances of these, with a base class of
+    /// packet_Base implementing a default constructor getting at least the name of the sender
+    /// (in the event it is called from WSARecv), or variations of SYSTEM or original_player_name
+    /// in the event of WSASend calls. Typically the name won't be used unless the data is already
+    /// known to be of a type where name is useful, so any irregularities are of no consequence.
+    /// </summary>
+    public class packet_Base
+    {
+        internal byte Type;
+        internal int PlayerId;
+        internal string Name;
+        internal byte[] Packet;
+
+        public packet_Base(byte[] data)
+        {
+            Packet = data;
+            Type = data[4];
+            PlayerId = data[5];
+            Name = Main.player[PlayerId].name;
+            if (Name == "")
+                Name = "SYSTEM";
+        }
+    }
+
+    /// <summary>
+    /// Chat Message/Broadcast/Announce Information
+    /// </summary>
+    public class packet_ChatMsg : packet_Base
+    {
+        internal string Text;
+        internal byte R;
+        internal byte G;
+        internal byte B;
+
+        internal packet_ChatMsg(byte[] data)
+            : base(data)
+        {
+            R = data[6];
+            G = data[7];
+            B = data[8];
+            Text = Encoding.ASCII.GetString(data, 9, data.Length - 9);
+        }
+
+    }
+
+    /// <summary>
+    /// Player State Information
+    /// </summary>
+    public class packet_PlayerState : packet_Base
+    {
+        internal Vector2 Position;
+        internal Vector2 Velocity;
+        internal int SelectedItemId;
+        internal int ButtonState;
+
+        internal packet_PlayerState(byte[] data)
+            : base(data)
+        {
+            ButtonState = data[6];
+            SelectedItemId = data[7];
+            Position.X = BitConverter.ToSingle(data, 8);
+            Position.Y = BitConverter.ToSingle(data, 12);
+            Velocity.X = BitConverter.ToSingle(data, 16);
+            Velocity.Y = BitConverter.ToSingle(data, 20);
+
+        }
+    }
+
+    /// <summary>
+    /// Packet struct, used to pass around data during processing for modification,
+    /// and passed back to WSASend. Requires marshalling before it can be used as 
+    /// the buffer for the final WSASend call
+    /// </summary>
+    public struct Packet
+    {
+        internal byte[] Data;
+        internal int Length;
+
+        internal Packet(byte[] d, int l)
+        {
+            Data = d;
+            Length = l;
+        }
+    }
+    #endregion
+}
--- RomTerraria/SockHook.cs
+++ RomTerraria/SockHook.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Windows.Forms;
 using EasyHook;
 using System.Net.Sockets;
-using Terraria;
-using Microsoft.Xna.Framework;
 
 
 namespace RomTerraria
@@ -23,16 +19,16 @@ namespace RomTerraria
         // Pointer to Buffer
     }
 
-    public partial class SockHook :  IEntryPoint
+    /// <summary>
+    /// The code in this class needs to be locked down, as any exception thrown here is completely
+    /// unraised, will fault the socket and cause the hook to drop with no warning. Any logic should
+    /// be in Commands.cs unless absolutely necessary.
+    /// </summary>
+    public class SockHook :  IEntryPoint
     {
 
         public LocalHook CreateWSARecvHook;
         public LocalHook CreateWSASendHook;
-
-        public SockHook()
-        {
-            
-        }
 
         #region dllimports
         [DllImport("Ws2_32.dll", CharSet=CharSet.Unicode, SetLastError=true)]
@@ -122,29 +118,22 @@ namespace RomTerraria
                 [In] IntPtr completionRoutine)
         {
 
-
-                var result = WSARecv(socketHandle, ref Buffer, BufferCount, bytesTransferred, ref socketFlags,
-                                     overlapped, completionRoutine);
-                if (result != 0)
-                {
-                    MakeItHarder.serverConsole.AddChatLine("Socket Error!");
-                    return result;
-                }
+            var result = WSARecv(socketHandle, ref Buffer, BufferCount, bytesTransferred, ref socketFlags,
+                                    overlapped, completionRoutine);
+            if (result != 0)
+            {
+                MakeItHarder.serverConsole.AddChatLine("Socket Error!");
+                return result;
+            }
 
             int bytes = Marshal.ReadInt32(bytesTransferred);
-                if (bytes > 0)
-                {
-
-                    var newBuffer = new byte[bytes];
-                    try
-                    {
-                        Marshal.Copy(Buffer.buf, newBuffer, 0, bytes);
-                    }
-                    catch (Exception e)
-                    {
-                        MessageBox.Show(e.ToString());
-                    }
-                    //processData(newBuffer);
+            if (bytes > 0)
+            {
+                var newBuffer = new byte[bytes];
+                Marshal.Copy(Buffer.buf, newBuffer, 0, bytes);
+                //processData(newBuffer); //processData calls on RECV will strictly be read-only.
+                //fairly simple, since it's designed around the Packet struct and ignoring that
+                //forces normal program flow.
 #region old_WSARecv_blocking_code.
                     //ok, this is fucking ridiculous. modifying the bytesTransferred ref should prevent processing by the application
                     //however probably .net or even xNA is fucking that up. going to intercept WSASend, so that when it tries to reply
@@ -189,8 +178,8 @@ namespace RomTerraria
                     */
 #endregion
 
-                }
-            return 0;
+            }
+            return result;
         }
 
         /// <summary>
@@ -236,211 +225,30 @@ namespace RomTerraria
             var newBuffer = new byte[Buffer.len]; //buffer to hold sent bytes
             Marshal.Copy(Buffer.buf,newBuffer,0,Buffer.len);
 
-            //test code, triggers on chat messages OUT from the server. 
-            //saves having to modify unmanaged code pointers and whatever other
-            //bullshit was stopping sane breaks in WSARecv logic (because for 
-            //WSASend we can just not call the original WSASend and return 0,
-            //effectively jumping over the socket code).
+            //I absolutely wish this code wasn't here, but unfortunately it's necessary.
             if (newBuffer[4] == 0x19)
             {
-                Packet n = ProcessData(newBuffer);
-                if (n.length == -1)
+                var n = Commands.ProcessData(newBuffer);
+                if (n.Length == -1)
                 {
                     return 0;
                 }
                 int i = 0;
-                foreach (byte b in n.data)
+                foreach (byte b in n.Data)
                 {
-                    Marshal.WriteByte(Buffer.buf, i, n.data[i]);
+                    Marshal.WriteByte(Buffer.buf, i, b);
                     i++;
                 }
-                // fake = GCHandle.Alloc(n.data, GCHandleType.Pinned);
-                //IntPtr fakePtr = fake.AddrOfPinnedObject();
-                //Marshal.WriteIntPtr(Buffer.buf, fakePtr);
-                Buffer.len = n.length;
+                Buffer.len = n.Length;
             }
 
-            var result = WSASend(socketHandle, ref Buffer, BufferCount, bytesTransferred, socketFlags, overlapped,
+            //call WSASend
+            return WSASend(socketHandle, ref Buffer, BufferCount, bytesTransferred, socketFlags, overlapped,
                                  completionRoutine);
-            return result;
-        }
-
-        /// <summary>
-        /// Process a data packet, determining data type and where defined, 
-        /// calling additional handlers to break apart data into a more workable
-        /// format.
-        /// </summary>
-        /// <param name="data">The data.</param>
-        public static Packet ProcessData(byte[] data)
-        {
-            byte type = data[4];
-
-            string prefix;
-            string details = null;
-            var packet = new Packet(data, data.Length);
-
-            switch (type)
-            {
-                case 0x01:
-                    prefix = "USER LOGIN P1";
-                    break;
-                case 0x02:
-                    prefix = "USER LOGIN RELATED";
-                    break;
-                case 0x04:
-                    prefix = "USER LOGIN RELATED [CHARACTER DETAILS]";
-                    break;
-                case 0x05:
-                    prefix = "PLAYER UPDATE INVENTORY/EQUIP";
-                    break;
-                case 0x06:
-                    prefix = "PLAYER SEND COMPLETE [LOGIN]";
-                    break;
-                case 0x08:
-                    prefix = "PLAYER REQUEST TILE DATA";
-                    break;
-                case 0x0C:
-                    prefix = "PLAYER SYNC/GREET REQUEST";
-                    break;
-                case 0x0D:
-                    prefix = "PLAYER STATE CHANGE";
-                    //packet = new packet_PlayerState(data);
-                    break;
-                case 0x10:
-                    prefix = "PLAYER CURRENT/MAX HEALTH UPDATE";
-                    break;
-                case 0x13:
-                    prefix = "PLAYER USE DOOR";
-                    break;
-                case 0x16:
-                    prefix = "DETERMINE ITEM OWNER";    //UPDATE: apparently item[x].active defines whether the item is
-                                                        //active within the game world (outside of inventories). I assume
-                                                        //the shit in FindOwner is to determine who the new owner of a newly
-                                                        //dropped item is (owner is the current dropper, so they don't have
-                                                        //have preference on the item by being closest). Also seems to be called
-                                                        //when you try to pick up an item you don't have room for, which explains
-                                                        //the erroneus captures I was recieving.
-
-                    break;
-                case 0x18:
-                    prefix = "PLAYER HURT NPC";
-                    break;
-                case 0x19:
-                    prefix = "CHAT MESSAGE";
-                    packet = HandleChatMsg(data);   //pass data to handler. 
-                                                    //ALL handlers assume a Packet struct as input.
-                                                    //and ALL handlers return a Packet struct.
-
-                    break;
-                case 0x1A:
-                    prefix = "PLAYER HURT PLAYER";
-                    break;
-                case 0x1B:
-                    prefix = "PLAYER FIRED PROJECTILE";
-                    break;
-                case 0x1E:
-                    prefix = "PLAYER TOGGLED PVP";
-                    break;
-                case 0x1F:
-                    prefix = "PLAYER USE CHEST";
-                    break;
-                case 0x20:
-                    prefix = "?PLAYER PLACED CHEST?";
-                    break;
-                case 0x23:
-                    prefix = "PLAYER HEAL EFFECT";
-                    break;
-                case 0x24:
-                    prefix = "PLAYER ZONE UPDATE";
-                    break;
-                case 0x26:
-                    prefix = "SEND PASSWORD";
-                    break;
-                case 0x28:
-                    prefix = "PLAYER INTERACT NPC";
-                    break;
-                case 0x2A:
-                    prefix = "PLAYER CURRENT/MAX MANA UPDATE";
-                    break;
-                case 0x2c:
-                    prefix = "PLAYER DIED";
-                    break;
-                case 0x2d:
-                    prefix = "PLAYER PARTY UPDATE";
-                    break;
-                case 0x2F:
-                    prefix = "PLAYER USE/PLACE SIGN";
-                    break;
-                case 0x31:
-                    prefix = "PLAYER SPAWN";
-                    break;
-
-                default:
-                    prefix = "UNDEFINED";
-                    break;
-            }
-
-            var sBuffer = new StringBuilder();
-            foreach (byte t in data)
-            {
-                sBuffer.Append(Convert.ToInt32(t).ToString("x").PadLeft(2, '0') + " ");
-            }
-            //string s = System.Text.Encoding.ASCII.GetString(newBuffer);
-            //MakeItHarder.serverConsole.AddChatLine(sBuffer.ToString().ToUpper());
-
-            MakeItHarder.serverConsole.AddChatLine(prefix + " : :" + sBuffer.ToString().ToUpper());
-       
-            return packet;
 
         }
 
-        private static Packet HandleChatMsg(byte[] data)
-        {
-            var p = new packet_ChatMsg(data); //initialize packet class, populate fields from data
-            //...
-            //act on packet fields, generate new data etc.
-            var match = false;
-            if (p.text[0] == 0x2F) // match backslash
-            {
-                
-                var commands = p.text.Split(' ');
-                switch (commands[0])
-                {
-                    case ("/meteor"):
-                        WorldEvents.SpawnMeteorCB();
-                        match = true;
-                        break;
-                    case ("/broadcast"):
-                        if (commands.Length > 1)
-                        {
-                            string msg = null;
-                            int i = 1;
-                            while (i<commands.Length)
-                            {
-                                msg = msg + " " + commands[i];
-                                i++;
-                            }
-                            Terraria.NetMessage.SendData(25, -1, -1, msg, 8, 0xff, 0xcc, 0x66);
-                        } else
-                        {
-                            Terraria.NetMessage.SendData(25, p.playerId, -1, "USAGE: /echo <word>", 8, 0x99, 0xff, 0x99);
-                        }
-                        match = true;
-                        break;
-                    default:
-                        match = false;
-                        break;
-                }
-
-            }
-            //create new Packet(data, length). use a length of -1 to indicate the packet is to be dropped.
-            //this will directly be used by WSASend so don't fuck it up!
-            //p.packet[10] = 0x7E;
-            if (match)
-                return new Packet(new byte[] {}, 0);
-
-            return new Packet(p.packet, p.packet.Length);
-        }
+        
 
 
         /// <summary>
@@ -469,92 +277,5 @@ namespace RomTerraria
 
     }
 
-    #region packet definitions
-    /// <summary>
-    /// classes for packet structures.
-    /// the packet parsing routines will create instances of these, with a base class of
-    /// packet_Base implementing a default constructor getting at least the name of the sender
-    /// (in the event it is called from WSARecv), or variations of SYSTEM or original_player_name
-    /// in the event of WSASend calls. Typically the name won't be used unless the data is already
-    /// known to be of a type where name is useful, so any irregularities are of no consequence.
-    /// </summary>
-    public class packet_Base
-    {
-        internal byte type;
-        internal int playerId;
-        internal string name;
-        internal byte[] packet;
 
-        public packet_Base(byte[] data)
-        {
-            packet = data;
-            type = data[4];
-            playerId = data[5];
-            name = Main.player[playerId].name;
-            if (name == "")
-                name = "SYSTEM";
-        }
-    }
-
-    /// <summary>
-    /// Chat Message/Broadcast/Announce Information
-    /// </summary>
-    public class packet_ChatMsg : packet_Base
-    {
-        internal string text;
-        internal byte R;
-        internal byte G;
-        internal byte B;
-
-        internal packet_ChatMsg(byte[] data)
-            : base(data)
-        {
-            R = data[6];
-            G = data[7];
-            B = data[8];
-            text = Encoding.ASCII.GetString(data, 9, data.Length - 9);
-        }
-
-    }
-
-    /// <summary>
-    /// Player State Information
-    /// </summary>
-    public class packet_PlayerState : packet_Base
-    {
-        internal Vector2 position;
-        internal Vector2 velocity;
-        internal int selectedItemId;
-        internal int buttonState;
-
-        internal packet_PlayerState(byte[] data)
-            : base(data)
-        {
-            buttonState = data[6];
-            selectedItemId = data[7];
-            position.X = BitConverter.ToSingle(data, 8);
-            position.Y = BitConverter.ToSingle(data, 12);
-            velocity.X = BitConverter.ToSingle(data, 16);
-            velocity.Y = BitConverter.ToSingle(data, 20);
-
-        }
-    }
-
-    /// <summary>
-    /// Packet struct, used to pass around data during processing for modification,
-    /// and passed back to WSASend. Requires marshalling before it can be used as 
-    /// the buffer for the final WSASend call
-    /// </summary>
-    public struct Packet
-    {
-        internal byte[] data;
-        internal int length;
-
-        internal Packet(byte[] d, int l)
-        {
-            data = d;
-            length = l;
-        }
-    }
-    #endregion
 }
